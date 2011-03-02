acdada321a6f8449566094f8a018a42661d53529
adamw <adam@warski.org>
2011-03-02 16:46:21 +0100

    An empty Rails app

---
+++ trqbox-demo-frontend/rails/trqbox-demo-frontend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrqboxDemoFrontend::Application.config.secret_token = '538e97ca99f78c1f2ddf113a162106281c7c995f2447a323bbbb06dc4e35dff803c90db5183e4797c7341fa8bcf80bfd7123bb932e7115acbd3b316c2cff1313'
