c413476f38db7dd21aaff0ea592976c62e13045c
adamw <adam@warski.org>
2011-03-02 16:17:04 +0100

    Hello world bean

---
+++ trqbox-demo-backend/src/main/java/pl/softwaremill/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package pl.softwaremill.demo;
+
+/**
+ * @author Adam Warski (adam at warski dot org)
+ */
+public class HelloWorld {
+    public String getMessage() {
+        return "Hello world!";
+    }
+}
