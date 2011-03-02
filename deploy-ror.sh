59311a1322fbfd6e233265377d6addf9e58570c7
adamw <adam@warski.org>
2011-03-02 16:46:41 +0100

    RoR deploy scripts

---
+++ deploy-ror.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ "$1" = "--skip-config" ]; then
+    echo "skip config"
+    echo "TORQUEBOX_HOME = $TORQUEBOX_HOME"
+    echo "PATH = $PATH"
+else
+    source conf_env
+fi
+
+export YML_FILE=$JBOSS_HOME/server/default/deploy/trqbox-demo-frontend-knob.yml
+if [ -e $YML_FILE ]; then
+	echo "Redeploy, touching"
+	touch $YML_FILE
+else
+	echo "First time deploy, running rake"
+	cd trqbox-demo-frontend/rails/trqbox-demo-frontend
+	jruby -S rake torquebox:deploy
+fi
