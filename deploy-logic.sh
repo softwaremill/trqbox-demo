0ee7189014acfba1c0888cab6f4208dba79ffda1
adamw <adam@warski.org>
2011-03-04 08:00:34 +0100

    No need to remove guava, it is not bundled

--- deploy-logic.sh
+++ deploy-logic.sh
@@ -7,4 +7,3 @@ export LIB_TARGET=trqbox-demo-frontend/rails/trqbox-demo-frontend/lib
 rm -rf $LIB_TARGET/*.jar
 
 cp trqbox-demo-ror-cdi-int/target/trqbox-demo-ror-cdi-int-1.0.0-SNAPSHOT/WEB-INF/lib/*.jar $LIB_TARGET
-rm $LIB_TARGET/guava-r07.jar
