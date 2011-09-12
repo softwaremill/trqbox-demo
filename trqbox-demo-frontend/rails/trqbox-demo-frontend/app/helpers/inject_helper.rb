d0ed322a98906bcf811a3ea9e4dcd393b4ee0732
Adam Warski <adam@warski.org>
2011-09-12 11:10:32 +0200

    Cleaning up

--- trqbox-demo-frontend/rails/trqbox-demo-frontend/app/helpers/inject_helper.rb
+++
@@ -1,7 +0,0 @@
-module InjectHelper
-  include_class "pl.softwaremill.common.util.dependency.D"
-
-  def lookup(what)
-    D.inject(what.java_class)
-  end
-end
--- trqbox-demo-frontend/rails/trqbox-demo-frontend/app/jobs/seconds_counter_invoker.rb
+++
@@ -1,9 +0,0 @@
-class SecondsCounterInvoker
-  #include InjectHelper
-
-  #include_class "pl.softwaremill.demo.SecondsCounter"
-
-  def run()
-    #lookup(SecondsCounter).inc
-  end
-end
